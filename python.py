e29ca2e5de8ef2d67eaea58b1f4752dafda228df
phamvanhung <phamvanhung2e123@gmail.com>
2013-12-17 03:01:15 +0900

    fix

---
+++ python.py
@@ -0,0 +1,27 @@
+import pymongo
+import sys
+connection = pymongo.Connection("mongodb://localhost", safe=True)
+db=connection.students
+grades = db.grades
+
+def delete_lowest_score():
+
+	print "grading nice?! look at these scores now!"
+
+	query = {'type':'homework'}
+	sort = [('student_id',pymongo.ASCENDING),('score',pymongo.ASCENDING)]
+	track = -1
+
+	try:
+		cursor = grades.find(query).sort(sort)
+
+	except:
+		print "Unexpected error:", sys.exc_info()[0]
+
+	for doc in cursor:
+		if doc['student_id'] != track:
+			grades.remove(doc)
+			track = doc['student_id']
+
+
+delete_lowest_score()
