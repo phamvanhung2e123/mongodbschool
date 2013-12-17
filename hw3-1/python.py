57c1cd563f49d24f6d75412c262d1d25aee05209
phamvanhung <phamvanhung2e123@gmail.com>
2013-12-17 09:05:00 +0900

    fix

--- hw3-1/python.py
+++ hw3-1/python.py
@@ -1,24 +1,36 @@
 import pymongo
 import sys
 import pprint
+from operator import itemgetter, attrgetter
+
 connection = pymongo.Connection("mongodb://localhost", safe=True)
-db=connection.school
+db = connection.school
 students = db.students
 
+
 def delete_lowest_score():
+    print "students nice?! look at these scores now!"
 
-	print "students nice?! look at these scores now!"
+    track = -1
 
-	track = -1
+    try:
+        cursor = students.find()
 
-	try:
-		cursor = grades.find()
+    except:
+        print "Unexpected error:", sys.exc_info()[0]
 
-	except:
-		print "Unexpected error:", sys.exc_info()[0]
-
-	for doc in cursor:
-		if doc['student_id'] != track:
+    for doc in cursor:
+            print "before"
             pprint.pprint(doc)
+            new_scores = sorted(doc["scores"],cmp = score_compare)
+            new_scores.pop()
+            doc["scores"]= new_scores
+            students.save(doc)
+            print "after"
+            pprint.pprint(students.find_one(doc))
+
+
+def score_compare(x, y):
+    return (int)(y["score"] -x["score"]);
 
 delete_lowest_score()
--- python.py
+++ python.py
@@ -1,27 +1,30 @@
 import pymongo
 import sys
+
 connection = pymongo.Connection("mongodb://localhost", safe=True)
-db=connection.students
+db = connection.students
 grades = db.grades
 
+
 def delete_lowest_score():
+    print "grading nice?! look at these scores now!"
 
-	print "grading nice?! look at these scores now!"
 
-	query = {'type':'homework'}
-	sort = [('student_id',pymongo.ASCENDING),('score',pymongo.ASCENDING)]
-	track = -1
 
-	try:
-		cursor = grades.find(query).sort(sort)
+    query = {'type': 'homework'}
+    sort = [('student_id', pymongo.ASCENDING), ('score', pymongo.ASCENDING)]
+    track = -1
 
-	except:
-		print "Unexpected error:", sys.exc_info()[0]
+    try:
+        cursor = grades.find(query).sort(sort)
 
-	for doc in cursor:
-		if doc['student_id'] != track:
-			grades.remove(doc)
-			track = doc['student_id']
+    except:
+        print "Unexpected error:", sys.exc_info()[0]
 
+    for doc in cursor:
+        if doc['student_id'] != track:
+            grades.remove(doc)
+            print "grading nice?! look at these scores now!"
+            track = doc['student_id']
 
 delete_lowest_score()
